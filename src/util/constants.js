6e95e1aad82c77ae8149d4faa93d2a6be93d50b3
Benjamin <benjamin.chen@yale.edu>
2022-10-05 21:13:06 -0400

    New linkedin url

--- src/util/constants.js
+++ src/util/constants.js
@@ -16,9 +16,8 @@ export const colors = {
 export const skills = ['ReactJS', 'Python', 'C Programming', 'Heroku', 'AWS', 'SQL', 'Postgres', 'Django', 'Bootstrap', 'JavaScript', 'Git', 
                         'NLP', 'REST', 'CSS', "Data Science", "Data Mining", "Web Development"]
 
-export const my_linkedin_url = 'https://www.linkedin.com/in/benjamin-chen-07402b142/'
+export const my_linkedin_url = 'https://www.linkedin.com/in/benjamin--chen/'
 export const my_github_url = 'https://github.com/BenTheNetizen'
-export const resume_url = 'https://alexander-academy-files.s3.us-east-2.amazonaws.com/Benjamin+Chen+Resume.pdf'
 
 export const projects = [
     {
